80a1aa1ed041d42619a4b06b1c2e20f65e98f80c
carrie-lyle <150395975+carrie-lyle@users.noreply.github.com>
2024-08-10 12:02:35 -0700

    update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib as plt
 import folium
 from streamlit_folium import folium_static
 from folium.plugins import HeatMap, MarkerCluster
